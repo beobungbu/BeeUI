0ae49f578b0dbcd62f141a9d207dbe1249e44114
Lan Tran Duc <lantranduc@gmail.com>
2026-08-21 18:36:56 +0700

    feat: add description list primitives

---
+++ packages/ui/src/components/description-list.tsx
@@ -0,0 +1,27 @@
+import { cn } from '@beeui/core';
+import * as React from 'react';
+import { View, type ViewProps } from 'react-native';
+import { MetadataRow, type MetadataRowProps } from './metadata-row';
+
+export type DescriptionListProps = ViewProps & {
+  className?: string;
+};
+
+export const DescriptionList = React.forwardRef<React.ComponentRef<typeof View>, DescriptionListProps>(
+  ({ className, ...props }, ref) => (
+    <View ref={ref} className={cn('w-full gap-0', className)} {...props} />
+  ),
+);
+
+DescriptionList.displayName = 'DescriptionList';
+
+export type DescriptionItemProps = MetadataRowProps;
+
+export const DescriptionItem = React.forwardRef<
+  React.ComponentRef<typeof View>,
+  DescriptionItemProps
+>(({ className, ...props }, ref) => (
+  <MetadataRow ref={ref} className={cn('border-b border-border last:border-b-0', className)} {...props} />
+));
+
+DescriptionItem.displayName = 'DescriptionItem';
