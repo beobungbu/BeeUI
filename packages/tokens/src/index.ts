756ecf112afa8b4837d992c98ef32d7d42f25301
Lan Tran Duc <lantranduc@gmail.com>
2026-08-24 15:57:59 +0700

    fix(tokens): preserve raised shadow visual value

--- packages/tokens/src/index.ts
+++ packages/tokens/src/index.ts
@@ -193,7 +193,7 @@ export const elevation = {
     nativeElevation: 0,
   },
   raised: {
-    web: '0 2px 8px rgb(16 24 40 / 0.10)',
+    web: '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)',
     nativeElevation: 2,
   },
   overlay: {
