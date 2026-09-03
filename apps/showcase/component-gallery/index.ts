af534a6fedf2145f23d9ccbc8c355758bf9df2b7
Lan Tran Duc <lantranduc@gmail.com>
2026-09-03 10:02:46 +0700

    fix(showcase): render public docs runtime fixtures

--- apps/showcase/component-gallery/index.ts
+++ apps/showcase/component-gallery/index.ts
@@ -1,1 +1,12 @@
-export { ComponentGallery } from './component-gallery';
+import * as React from 'react';
+import { ComponentGallery as BaseComponentGallery } from './component-gallery';
+import { PublicDocFixtures } from './public-doc-fixtures';
+
+export function ComponentGallery() {
+  return (
+    <>
+      <BaseComponentGallery />
+      <PublicDocFixtures />
+    </>
+  );
+}
