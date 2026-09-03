3a325373398a92a330e5258527e4c8f73f356e19
Lan Tran Duc <lantranduc@gmail.com>
2026-09-03 10:05:41 +0700

    fix(showcase): keep gallery wrapper valid TypeScript

--- apps/showcase/component-gallery/index.ts
+++ apps/showcase/component-gallery/index.ts
@@ -3,10 +3,10 @@ import { ComponentGallery as BaseComponentGallery } from './component-gallery';
 import { PublicDocFixtures } from './public-doc-fixtures';
 
 export function ComponentGallery() {
-  return (
-    <>
-      <BaseComponentGallery />
-      <PublicDocFixtures />
-    </>
+  return React.createElement(
+    React.Fragment,
+    null,
+    React.createElement(BaseComponentGallery),
+    React.createElement(PublicDocFixtures),
   );
 }
