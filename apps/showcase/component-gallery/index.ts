610673acdb5df9c94b57445aeb7fc1d03f73d52f
Lan Tran Duc <lantranduc@gmail.com>
2026-09-03 10:45:32 +0700

    fix(showcase): keep public doc fixtures out of gallery runtime

--- apps/showcase/component-gallery/index.ts
+++ apps/showcase/component-gallery/index.ts
@@ -1,14 +1,2 @@
-import * as React from 'react';
-import { ComponentGallery as BaseComponentGallery } from './component-gallery';
-import { PublicDocFixtures } from './public-doc-fixtures';
-
-type ComponentGalleryProps = React.ComponentProps<typeof BaseComponentGallery>;
-
-export function ComponentGallery(props: ComponentGalleryProps) {
-  return React.createElement(
-    React.Fragment,
-    null,
-    React.createElement(BaseComponentGallery, props),
-    React.createElement(PublicDocFixtures),
-  );
-}
+export { ComponentGallery } from './component-gallery';
+export { PublicDocFixtures } from './public-doc-fixtures';
